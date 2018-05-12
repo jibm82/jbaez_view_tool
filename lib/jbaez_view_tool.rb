b87422e6c146dd02720fe9afb78ca90e579472e7
Jorge Baez <jibm_82@yahoo.com.mx>
2018-05-11 19:56:48 -0500

    Dev tool initial commit

---
+++ lib/jbaez_view_tool.rb
@@ -0,0 +1,6 @@
+require "jbaez_view_tool/version"
+require "jbaez_view_tool/renderer"
+
+module JbaezViewTool
+  # Your code goes here...
+end
---
+++ spec/jbaez_view_tool_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe JbaezViewTool do
+  it "has a version number" do
+    expect(JbaezViewTool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
